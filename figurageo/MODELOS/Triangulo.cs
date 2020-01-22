d5874a26cf1527a4d0b01b29b158a696f8d18fd9
Armando Daniel Barrera Serna <0206941@up.edu.mx>
2020-01-21 19:53:46 -0600

    Initial commit

---
+++ figurageo/MODELOS/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace figurageo.MODELOS
+{
+    public class Triangulo
+    {
+        public Triangulo()
+        {
+        }
+    }
+}
---
+++ figurageo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace figurageo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
