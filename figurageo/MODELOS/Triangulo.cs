3b62055c80eb1c1318dd50f40d1df2f9e3caf17d
Armando Daniel Barrera Serna <0206941@up.edu.mx>
2020-01-21 19:54:00 -0600

    ok

--- figurageo/MODELOS/Triangulo.cs
+++ figurageo/MODELOS/Triangulo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 namespace figurageo.MODELOS
 {
-    public class Triangulo
+    public class Triangulo: Figura
     {
         public Triangulo()
         {
+            _atributo1 = 2;
+            _atributo3 = 3;
         }
     }
 }
--- figurageo/Program.cs
+++ figurageo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using figurageo.MODELOS;
 
 namespace figurageo
 {
@@ -6,7 +7,13 @@ namespace figurageo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            var figura = new Figura();
+            figura._atributo1 = 2;
+
+            var triangulo = new Triangulo();
+            triangulo._atributo1 = 1;
+
+            Console.WriteLine($"El valor del atributo 1 es: {figura._atributo1}");
+        }   
     }
 }
